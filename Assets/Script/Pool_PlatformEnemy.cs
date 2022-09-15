e5e84fad7f205bbce3046c8fffeb50f9a1c5806d
ofgonen <ofgonen@gmail.com>
2022-09-16 00:31:29 +0300

    Pool_PlatformEnemy: Remove spawn method

--- Assets/Script/Pool_PlatformEnemy.cs
+++ Assets/Script/Pool_PlatformEnemy.cs
@@ -9,8 +9,4 @@ using FFStudio;
 [ CreateAssetMenu( fileName = "pool_enemy_platform", menuName = "FF/Data/Pool/Platform Enemy" ) ]
 public class Pool_PlatformEnemy : ComponentPool< PlatformEnemy >
 {
-    public void Spawn( Vector3 position, Vector3 forward )
-    {
-		GetEntity().Spawn( position, forward );
-	}
 }
