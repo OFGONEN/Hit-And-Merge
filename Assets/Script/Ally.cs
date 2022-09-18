7e84fb3841d56bb2ad6cadf3cc005fedf85cba2f
ofgonen <ofgonen@gmail.com>
2022-09-18 14:04:25 +0300

    Create Ally & Pool_Ally

---
+++ Assets/Script/Ally.cs
@@ -0,0 +1,32 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+using Sirenix.OdinInspector;
+
+public class Ally : MonoBehaviour
+{
+#region Fields
+  [ Title( "Shared Variables" ) ]
+    [ SerializeField ] Pool_Ally pool_ally;
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
