4e45a0dff262167c95e68710d12526d97cdee852
ofgonen_office <ofgonen@gmail.com>
2022-09-27 20:22:52 +0300

    SON COMMIT

--- Assets/Script/UIIncrementalButtonManPower.cs
+++ Assets/Script/UIIncrementalButtonManPower.cs
@@ -39,8 +39,8 @@ public class UIIncrementalButtonManPower : MonoBehaviour
 #region API
     public void Configure()
     {
-		incremental_index = PlayerPrefsUtility.Instance.GetInt( ExtensionMethods.Key_Incremental_ManPower, 0 );
-		incremental       = incremental_manPower.ReturnIncrementalAtIndex( incremental_index );
+		incremental_index = PlayerPrefsUtility.Instance.GetInt( ExtensionMethods.Key_Incremental_ManPower, -1 );
+		incremental       = incremental_manPower.ReturnIncrementalAtIndex( incremental_index + 1 );
 
 		text_value.text = incremental.incremental_man + " Man";
 		text_cost.text  = incremental.incremental_cost.ToString();
@@ -55,8 +55,6 @@ public class UIIncrementalButtonManPower : MonoBehaviour
 
 	public void OnButtonPress()
 	{
-		PlayerPrefsUtility.Instance.SetInt( ExtensionMethods.Key_Incremental_ManPower, incremental_index );
-		event_incremental_power.Raise();
 		incremental_index++;
 
 		PlayerPrefsUtility.Instance.SetInt( ExtensionMethods.Key_Incremental_ManPower, incremental_index );
